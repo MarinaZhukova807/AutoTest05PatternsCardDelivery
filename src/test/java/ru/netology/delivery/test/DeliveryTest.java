1776fc4281744f8ef9c22d9bb57d90f5595ca0a8
Марина Жукова <marinazhukova807@gmail.com>
2022-08-27 17:48:59 +0300

    написаны негативные кейсы

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -13,6 +13,8 @@ import java.time.Duration;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 import static ru.netology.delivery.data.DataGenerator.Registration.generateUser;
+import static ru.netology.delivery.data.DataGenerator.generateName;
+import static ru.netology.delivery.data.DataGenerator.generatePhone;
 
 class DeliveryTest {
 
@@ -56,6 +58,7 @@ class DeliveryTest {
         $("[data-test-id=success-notification] .notification__title").shouldHave(exactText("Успешно!")).shouldBe(Condition.visible);
         $("[data-test-id=success-notification] .notification__content").shouldHave(exactText("Встреча успешно запланирована на " + firstMeetingDate)).shouldBe(Condition.visible);
     }
+
     @Test
     @DisplayName("Should successful plan and replan meeting")
     void shouldSuccessfulPlanAndReplanMeeting() {
@@ -87,7 +90,171 @@ class DeliveryTest {
         form.$("[data-test-id=date] input").setValue(secondMeetingDate);
         $$(".form-field .button").find(exactText("Запланировать")).click();
         $("[data-test-id=replan-notification] .notification__title").shouldHave(exactText("Необходимо подтверждение")).shouldBe(Condition.visible);
-        //$("[data-test-id=replan-notification] .notification__content").shouldHave(exactText("У вас уже запланирована встреча на другую дату. Перепланировать?")).shouldBe(Condition.visible);
+        $("[data-test-id=replan-notification] .notification__content").shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?")).shouldBe(visible);
         $$("[data-test-id=replan-notification] button .button__text").find(exactText("Перепланировать")).click();
+        $("[data-test-id=success-notification] .notification__title").shouldHave(exactText("Успешно!")).shouldBe(Condition.visible);
+        $("[data-test-id=success-notification] .notification__content").shouldHave(exactText("Встреча успешно запланирована на " + secondMeetingDate)).shouldBe(Condition.visible);
+    }
+
+    @Test
+    void emptyCityField(){
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        DataGenerator.UserInfo validUser = generateUser("ru");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=city] input").setValue("");
+        form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(firstMeetingDate);
+        form.$("[data-test-id=name] input").setValue(validUser.getName());
+        form.$("[data-test-id=phone] input").setValue(validUser.getPhone());
+        form.$("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id=city].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void notDictionaryCity(){
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        DataGenerator.UserInfo validUser = generateUser("ru");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=city] input").setValue("Клин");
+        form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(firstMeetingDate);
+        form.$("[data-test-id=name] input").setValue(validUser.getName());
+        form.$("[data-test-id=phone] input").setValue(validUser.getPhone());
+        form.$("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id=city].input_invalid .input__sub").shouldHave(exactText("Доставка в выбранный город недоступна"));
+    }
+    @Test
+    void setEmptyDateDelivery(){
+        DataGenerator.UserInfo validUser = generateUser("ru");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=city] input").setValue(validUser.getCity());
+        form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=name] input").setValue(validUser.getName());
+        form.$("[data-test-id=phone] input").setValue(validUser.getPhone());
+        form.$("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id=date] .input_invalid .input__sub").shouldHave(exactText("Неверно введена дата"));
+    }
+    @Test
+    void setErrorDateDelivery(){
+        DataGenerator.UserInfo validUser = generateUser("ru");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=city] input").setValue(validUser.getCity());
+        form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue("19.08.2022");
+        form.$("[data-test-id=name] input").setValue(validUser.getName());
+        form.$("[data-test-id=phone] input").setValue(validUser.getPhone());
+        form.$("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id=date] .input_invalid .input__sub").shouldHave(exactText("Заказ на выбранную дату невозможен"));
+    }
+    @Test
+    void setEmptyName(){
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        DataGenerator.UserInfo validUser = generateUser("ru");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=city] input").setValue(validUser.getCity());
+        form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(firstMeetingDate);
+        form.$("[data-test-id=name] input").setValue("");
+        form.$("[data-test-id=phone] input").setValue(validUser.getPhone());
+        form.$("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+    @Test
+    void setNotRussianName(){
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        DataGenerator.UserInfo validUser = generateUser("ru");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=city] input").setValue(validUser.getCity());
+        form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(firstMeetingDate);
+        form.$("[data-test-id=name] input").setValue(generateName("en"));
+        form.$("[data-test-id=phone] input").setValue(validUser.getPhone());
+        form.$("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+    @Test
+    void setNameWithSpacialChars(){
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        DataGenerator.UserInfo validUser = generateUser("ru");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=city] input").setValue(validUser.getCity());
+        form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(firstMeetingDate);
+        form.$("[data-test-id=name] input").setValue("иван% иванов");
+        form.$("[data-test-id=phone] input").setValue(validUser.getPhone());
+        form.$("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+    @Test
+    void setEmptyPhone(){
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        DataGenerator.UserInfo validUser = generateUser("ru");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=city] input").setValue(validUser.getCity());
+        form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(firstMeetingDate);
+        form.$("[data-test-id=name] input").setValue(validUser.getName());
+        form.$("[data-test-id=phone] input").setValue("");
+        form.$("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+    /*@Test
+    //в этой версии приложения отсутствует проверка на корректность телефона
+    void setErrorPhoneFormat(){
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        DataGenerator.UserInfo validUser = generateUser("ru");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=city] input").setValue(validUser.getCity());
+        form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(firstMeetingDate);
+        form.$("[data-test-id=name] input").setValue(validUser.getName());
+        form.$("[data-test-id=phone] input").setValue("1234567");
+        form.$("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }*/
+    @Test
+    void notCheckAgreements(){
+        var daysToAddForFirstMeeting = 8;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        DataGenerator.UserInfo validUser = generateUser("ru");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=city] input").setValue(validUser.getCity());
+        form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(firstMeetingDate);
+        form.$("[data-test-id=name] input").setValue(validUser.getName());
+        form.$("[data-test-id=phone] input").setValue(validUser.getPhone());
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id=agreement].input_invalid").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
+    }
+    @Test
+    void notFilled(){
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id=city].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 }
